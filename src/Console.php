2020eee61564d7141a9219581cc3e0a2be55f695
imacme <imacme@aliyun.com>
2019-02-16 10:43:56 +0800

    1、完善短网址生成接口代码

--- src/Console.php
+++ src/Console.php
@@ -9,7 +9,12 @@
 namespace Doacme\BaiduShorturl;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
 
+/**
+ * Class Console
+ * @package Doacme\BaiduShorturl
+ */
 class Console
 {
     /**
@@ -18,18 +23,18 @@ class Console
      */
     private static $store = [
         'client'=>null,
-        'token'=>null,
     ];
 
     private static $config = [
         'host'=>'https://dwz.cn',
         'createUrl'=>'/admin/v2/create',
         'queryUrl'=>'/admin/v2/query',
+        'token'=>null,
     ];
 
     /**
      * Console constructor.
-     * @param $token
+     * @param string $token
      */
     public function __construct($token)
     {
@@ -37,7 +42,7 @@ class Console
         if (!is_string($token)) {
             $token = '';
         }
-        self::$store['token'] = $token;
+        self::$config['token'] = $token;
     }
 
     /**
@@ -47,13 +52,68 @@ class Console
     private function client()
     {
         if (!isset(self::$store['client'])) {
-            self::$store['client'] = new Client(['verify'=>false]);
+            self::$store['client'] = new Client([
+                'verify'=>false,
+                'base_uri'=>self::$config['host'],
+            ]);
         }
         return self::$store['client'];
     }
 
-    public function create()
+    /**
+     * 短网址生成方法
+     * @param $url
+     * @return string
+     * @throws \Exception
+     */
+    public function create($url)
     {
-        $response = $this->client()->post('','');
+        try {
+            $response = $this->client()->post(self::$config['createUrl'], [
+                'headers' => [
+                    'Accept'=>'application/json',
+                    'Token'=>self::$config['token'],
+                ],
+                'form_params' => [
+                    'url'=>$url,
+                ],
+            ]);
+            $contents = $this->handleResponseOrFail($response);
+            $contents = json_decode($contents, true);
+            /**返回数据的格式
+            {
+            "Code": 0,
+            "ShortUrl": "https://dwz.cn/de3rp2Fl",
+            "LongUrl": "http://www.baidu.com",
+            "ErrMsg": ""
+            }
+             */
+            if (isset($contents['Code'])) {
+                if ($contents['Code'] == 0 && !empty($contents['ShortUrl'])) {
+                    return $contents['ShortUrl'];
+                }
+            }
+            throw new \Exception('获取短网址失败');
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    /**
+     * 统一处理请求响应
+     * @param $response
+     * @return string
+     * @throws \Exception
+     */
+    private function handleResponseOrFail($response)
+    {
+        if ($response->getResponseStatus() == 200) {
+            $contents = $response->getBody()->getContents();
+            if (is_string($contents) && !empty($contents)) {
+                return $contents;
+            }
+        } else {
+            throw new \Exception('网络请求异常');
+        }
     }
 }
